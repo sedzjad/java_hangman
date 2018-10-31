59fc7abc5edb3679a174471729740345941274b4
joshuavanderpoll <joshuavanderpoll@10.120.0.40>
2018-10-31 18:19:33 +0100

    Checking for chooses char

--- Hangman/src/Main.java
+++ Hangman/src/Main.java
@@ -1,10 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
+import java.util.Scanner;
 
 public class Main {
 
 	static String awnser = "";
 	static String hidden = "";
 	static int stage = 13;
+	static Scanner scan = new Scanner(System.in);
+	static List<String> guesslist = new ArrayList<>();
 
 	static String[] wordlist = { "Kaas", "Melk", "Boter", "Appel", "Wortel", "Nederland", "Duitsland", "Peer", "Aardbij",
 			"Fiets", "Trein", "Vliegtuig", "Fabriek", "Boederij", "Schoolgebouw", "Personeel", "Stage", "Tafels",
@@ -14,7 +19,73 @@ public class Main {
 			"Oplaad Kabel", "Kapper", "NS Trein", "Dubai City", "White Board", "OV Kaart" };
 
 	public static void main(String[] args) {
-		getWord();
+		game();
+	}
+	
+	public static void game() {
+		System.out.println("Wilt u spelen? (ja/nee)");
+		String vraag = scan.nextLine();
+		if ((vraag).equalsIgnoreCase("ja")) {
+			System.out.println("============================================================");
+			System.out.println("Welkom bij galgje.");
+			System.out.println("U heeft 12 levens om het woord letter voor letter te raden.");
+			System.out.println("Als u dit lukt wint u het spel en anders verliest u.");
+			System.out.println("============================================================");
+			getWord();
+			System.out.println();
+			chooseLetter();
+		} else {
+			System.out.println("Tot ziens!");
+			resetGame();
+		}
+	}
+	
+	public static void chooseLetter() {
+		System.out.println();
+		System.out.println("Please enter a letter:");
+		String vraag = scan.nextLine();
+		if(!vraag.equals("") && !vraag.equals(" ")) {
+			if(vraag.length() > 1) {
+				System.out.println("You are only allowed to use 1 character");
+				chooseLetter();
+				return;
+			}
+			if(guesslist.contains(vraag.toUpperCase())) {
+				System.out.println("U heeft de letter, " + vraag + " al gebruikt!");
+			} else {
+				if(awnser.toUpperCase().contains(vraag.toUpperCase())) {
+					System.out.println();
+					System.out.println();
+					System.out.println();
+					System.out.println();
+					guesslist.add(vraag.toUpperCase());
+					System.out.println("De letter, " + vraag + " is correct!");
+					getASCII(stage);
+					System.out.println(hidden);
+				} else {
+					System.out.println();
+					System.out.println();
+					System.out.println();
+					System.out.println();
+					guesslist.add(vraag.toUpperCase());
+					System.out.println("De letter, " + vraag + " is incorrect!");
+					stage = stage - 1;
+					getASCII(stage);
+					System.out.println(hidden);
+				}
+			}
+		}
+		chooseLetter();
+	}
+	
+	public static void resetGame() {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		game();
 	}
 
 	public static void getWord() {
